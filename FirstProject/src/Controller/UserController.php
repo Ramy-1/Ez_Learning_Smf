b0decb4a0e4658aa15250185502379fa418d8695
Ramy <mouhamedrami.bendhia@esprit.tn>
2022-04-03 23:33:40 +0100

    home + user route

--- FirstProject/src/Controller/UserController.php
+++ FirstProject/src/Controller/UserController.php
@@ -17,4 +17,18 @@ class UserController extends AbstractController
             'controller_name' => 'UserController',
         ]);
     }
+    /**
+     * @IsGranted("ROLE_ADMIN")
+     * @param UserRepository $repository
+     * @return Response
+     * @Route ({"/","/afficheuser"},name="afficheuser")
+     */
+    // public function afficheuser(UserRepository $repository)
+    // {
+    //     //$repository=$this->getDoctrine()->getRepository(Classroom::class);
+    //     $tabuser=$repository->findAll();
+    //     return $this->render('user/afficheuser.html.twig',[
+    //         'tab'=>$tabuser
+    //     ]);
+    // }
 }
