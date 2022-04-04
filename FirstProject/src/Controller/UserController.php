ab27577846f93ecc9ee37d4416fc4e4aa9524e70
Ramy <mouhamedrami.bendhia@esprit.tn>
2022-04-04 01:05:48 +0100

    awel il add user

--- FirstProject/src/Controller/UserController.php
+++ FirstProject/src/Controller/UserController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\UserType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class UserController extends AbstractController
 {
@@ -31,5 +34,23 @@ class UserController extends AbstractController
         return $this->render('user/index.html.twig',[
             'tab'=>$tabuser
         ]); 
+    }
+
+    /**
+     * @Route ("/updateuser/{id}" , name="updateuser")
+     */
+    public function update($id,UserRepository $repository ,Request $request)
+    {
+        $user=$repository->find($id);
+        $form=$this->createForm(UserType::class, $user);
+        $form->add('update',SubmitType::class);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $em=$this->getDoctrine()->getManager();
+            $em->flush();
+            return $this->redirectToRoute('afficheuser');
+        }
+        return $this->render('user/newUser.html.twig',['form'=>$form->createView()]);
     }
 }
