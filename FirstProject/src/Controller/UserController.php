4937d10773e7d3451f0808048905a25bf8b7dc28
Ramy <mouhamedrami.bendhia@esprit.tn>
2022-04-08 02:13:03 +0100

    tbc

--- FirstProject/src/Controller/UserController.php
+++ FirstProject/src/Controller/UserController.php
@@ -151,4 +151,14 @@ class UserController extends AbstractController
         }
         return $this->render('user/newUser.html.twig',['UserForm'=>$form->createView()]);
     }
+    // public function hash(String string) {
+    //     const utf8 = new TextEncoder().encode(string);
+    //     return crypto.subtle.digest('SHA-256', utf8).then((hashBuffer) => {
+    //       const hashArray = Array.from(new Uint8Array(hashBuffer));
+    //       const hashHex = hashArray
+    //         .map((bytes) => bytes.toString(16).padStart(2, '0'))
+    //         .join('');
+    //       return hashHex;
+    //     });
+    //   }
 }
