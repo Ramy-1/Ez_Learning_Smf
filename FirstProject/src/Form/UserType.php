f5d0f41beebc98296b29c79b422f78543983f638
Ramy <mouhamedrami.bendhia@esprit.tn>
2022-04-09 21:05:11 +0100

    ROLES !!!!!

--- FirstProject/src/Form/UserType.php
+++ FirstProject/src/Form/UserType.php
@@ -4,8 +4,12 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+
+
+use Symfony\Component\Form\CallbackTransformer;
 
 class UserType extends AbstractType
 {
@@ -13,13 +17,38 @@ class UserType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('roles')
+            // ->add('roles')
+            ->add('roles', ChoiceType::class, [
+                'required' => true,
+                'multiple' => false,
+                'expanded' => false,
+                'choices' => [
+                    'Admin' => 'ROLE_ADMIN',
+                    'Etudiant' => 'ROLE_ETUDIANT',
+                    'RECRUTEUR' => 'ROLE_RECRUTEUR',
+                    'Ensiegnant' => 'ROLE_ENSIEGNANT',
+                ],
+                // 'expanded' => true
+            ])
             ->add('password')
             ->add('name')
             ->add('lastName')
             ->add('faceID')
-            ->add('isVerified')
-        ;
+            ->add('isVerified');
+
+
+        // Data transformer
+        $builder->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($rolesArray) {
+                    // transform the array to a string
+                    return count($rolesArray) ? $rolesArray[0] : null;
+                },
+                function ($rolesString) {
+                    // transform the string back to an array
+                    return [$rolesString];
+                }
+            ));
     }
 
     public function configureOptions(OptionsResolver $resolver): void
