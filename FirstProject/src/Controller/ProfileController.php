46a60728df96ea76d72b38dfec98d32b8c977ec5
Ramy <mouhamedrami.bendhia@esprit.tn>
2022-04-12 04:17:29 +0100

    profile and settings ✔

---
+++ FirstProject/src/Controller/ProfileController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+use App\Form\UserType;
+use App\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class ProfileController extends AbstractController
+{
+    /**
+     * @Route("/profile", name="app_profile")
+     */
+    public function index(): Response
+    {
+        return $this->render('profile/index.html.twig', [
+            'user' => $this->getUser(),
+        ]);
+    }
+    /**
+     * @Route("/profile/settings", name="app_profiles")
+     */
+    public function settings(Request $request): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(UserType::class, $user);
+        $form->add('update', SubmitType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+            return $this->redirectToRoute('app_profile');
+        }
+        return $this->render('profile/settings.html.twig', [
+            'UserForm' => $form->createView(),
+        ]);
+    }
+}
