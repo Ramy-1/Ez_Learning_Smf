d2169ecd84e8530fe3a6de47e399472306472b30
Ramy <mouhamedrami.bendhia@esprit.tn>
2022-05-13 04:17:59 +0100

    .new Password
    qhdulbvrumnyifqn

--- FirstProject/src/Controller/RegistrationController.php
+++ FirstProject/src/Controller/RegistrationController.php
@@ -178,7 +178,6 @@ class RegistrationController extends AbstractController
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
             );
-            // do anything else you need here, like send an email
 
             return $guardHandler->authenticateUserAndHandleSuccess(
                 $user,
